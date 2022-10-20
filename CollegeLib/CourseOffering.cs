128e382c2693a356a396427485d59d4257a94a9a
Olivier Lefebvre <oli_43@hotmail.fr>
2022-10-20 14:49:21 -0400

    course compare to

--- CollegeLib/CourseOffering.cs
+++ CollegeLib/CourseOffering.cs
@@ -1,6 +1,6 @@
 ﻿namespace CollegeLib;
 
-public class CourseOffering
+public class CourseOffering : IComparable<CourseOffering>
 {
     public int Year { get; set; }
     public string Semester { get; set; }
@@ -14,5 +14,10 @@ public class CourseOffering
         Semester = semester;
         Course = course;
         Teacher = teacher;
+    }
+
+    public int CompareTo(CourseOffering? other)
+    {
+        throw new NotImplementedException();
     }
 }
