84af5b498ff1e9bbaba6a2f851958826a06af024
Olivier Lefebvre <oli_43@hotmail.fr>
2022-10-26 14:56:23 -0400

    all Sort done

--- CollegeDataLib/Sorting.cs
+++ CollegeDataLib/Sorting.cs
@@ -22,5 +22,54 @@ public static class Sorting
         }
 
     }
+    public static void SortPersonByDefault(List<Person> data, SortOrder sortOrder = SortOrder.Asc)
+    {
+        data.Sort();
+        if (sortOrder != SortOrder.Asc)
+        {
+            data.Reverse();
+        }
+    }
     
+    public static void SortPersonByDOB(List<Person> data, SortOrder sortOrder = SortOrder.Asc)
+    {
+        if (sortOrder == SortOrder.Asc)
+        {
+            data.Sort((x, y) => x.Dob.CompareTo(y.Dob));    
+        }
+        else
+        {
+            data.Sort((x, y) => y.Dob.CompareTo(x.Dob));
+        }
+    }
+    
+    public static void SortCourseByDefault(List<Course> data, SortOrder sortOrder = SortOrder.Asc)
+    {
+        data.Sort();
+        if (sortOrder != SortOrder.Asc)
+        {
+            data.Reverse();
+        }
+    }
+    
+    public static void SortCourseByName(List<Course> data, SortOrder sortOrder = SortOrder.Asc)
+    {
+        if (sortOrder == SortOrder.Asc)
+        {
+            data.Sort((x, y) => String.Compare(x.Name, y.Name, StringComparison.Ordinal));    
+        }
+        else
+        {
+            data.Sort((x, y) => String.Compare(y.Name, x.Name, StringComparison.Ordinal));
+        }
+    }
+    
+    public static void SortCourseOfferingByDefault(List<CourseOffering> data, SortOrder sortOrder = SortOrder.Asc)
+    {
+        data.Sort();
+        if (sortOrder != SortOrder.Asc)
+        {
+            data.Reverse();
+        }
+    }
 }
