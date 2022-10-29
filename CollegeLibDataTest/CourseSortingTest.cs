1ae4ec3f4abfbb8a49c7abc09fd0352ac0151be6
Olivier Lefebvre <oli_43@hotmail.fr>
2022-10-29 17:28:47 -0400

    add CourseSortingTest file

---
+++ CollegeLibDataTest/CourseSortingTest.cs
@@ -0,0 +1,16 @@
+﻿using CollegeDataLib;
+using CollegeLib;
+
+namespace CollegeLibDataTest;
+
+public class CourseSortingTest
+{
+    private readonly MockCourseRepo _mock;
+
+    public CourseSortingTest()
+    {
+        _mock = new MockCourseRepo();
+    }
+    
+    
+}
